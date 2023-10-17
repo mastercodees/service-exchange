ba2c96ab3c2561b76b82a6c6f7a75485b680c84e
preston176 <prestonnyamweya@gmail.com>
2023-10-17 15:13:38 +0300

    features numbered center

--- src/Components/How.jsx
+++ src/Components/How.jsx
@@ -8,21 +8,21 @@ function How() {
       <div className="wrapper">
         <div className="step">
             <div className="circle">
-                1
+                <p>1</p>
             </div>
             <h3>Eat</h3>
             <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <div className="step">
             <div className="circle">
-                1
+                <p>2</p>
             </div>
             <h3>Eat</h3>
             <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <div className="step">
             <div className="circle">
-                1
+                <p>3</p>
             </div>
             <h3>Eat</h3>
             <p>Lorem ipsum dolor sit amet.</p>
