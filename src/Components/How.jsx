5f78afc05b2f368b53c80ef6ebf9de083a43545e
mastercodes <vladmirnyataige@gmail.com>
2023-10-17 12:18:38 +0000

    home and how

---
+++ src/Components/How.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./How.css"
+
+function How() {
+  return (
+    <div className='how'>
+        <h2>How it works ...</h2>
+      <div className="wrapper">
+        <div className="step">
+            <div className="circle">
+                1
+            </div>
+            <h3>Eat</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="step">
+            <div className="circle">
+                1
+            </div>
+            <h3>Eat</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="step">
+            <div className="circle">
+                1
+            </div>
+            <h3>Eat</h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default How
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import "./Navbar.css"
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBIcon,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
+
+
+export default function App() {
+  const [showNavSecond, setShowNavSecond] = useState(false);
+
+  return (
+    <MDBNavbar expand='lg' light bgColor='light' className='nav'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Service Exchange</MDBNavbarBrand>
+        <MDBNavbarToggler
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavSecond(!showNavSecond)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNavSecond}>
+          <MDBNavbarNav>
+            <MDBNavbarLink active aria-current='page' href='#'>
+              Home
+            </MDBNavbarLink>
+            <MDBNavbarLink href='#'>About</MDBNavbarLink>
+            <MDBNavbarLink href='#'>Contact</MDBNavbarLink>
+            <MDBNavbarLink href='#'>Login</MDBNavbarLink>
+           
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
